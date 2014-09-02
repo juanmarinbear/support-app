36419f67227c2138c4309f9b5baeaf604240de32
Juan Marin <juan@codecery.com>
2014-09-02 17:05:48 -0500

    Connects options in scope with template.

--- app/create/create.js
+++ app/create/create.js
@@ -1,6 +1,24 @@
 myApp.controller('CreateCtrl', ['$scope', function($scope) {
   $scope.data = {};
 
+  $scope.subject = [
+    { label: 'Issue', value: 'issue' },
+    { label: 'Complaint', value: 'complaint' },
+    { label: 'Suggestion', value: 'suggestion' }
+  ];
+
+  $scope.service = [
+    { label: 'Residential', value: 'residential' },
+    { label: 'Business', value: 'business' },
+    { label: 'Dedicated', value: 'dedicated' }
+  ];
+
+  $scope.issue = [
+    { label: 'Disconnection', value: 'disconnection' },
+    { label: 'Slugginess', value: 'slugginess' },
+    { label: 'Other', value: 'other' }
+  ];
+
   $scope.submit = function() {
     console.log("Submit!");
   }
