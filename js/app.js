5d2f1fa3b699370d8487ca6d91bcde1f9d65b2d7
Juan Marin <juan@codecery.com>
2014-09-02 16:18:20 -0500

    Adds create and update routes.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/create");
+
+  $stateProvider
+    .state('create', {
+      url: "/create",
+      controller: 'CreateCtrl',
+      templateUrl: "create/create.tpl.html"
+    })
+    .state('update', {
+      url: "/update",
+      controller: 'UpdateCtrl',
+      templateUrl: "update/update.tpl.html"
+    });
+});
